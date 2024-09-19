8877e15face4aeb7fdfa4635d2b529857733399f
Anurag K Ray <anuragannu309@gmail.com>
2024-09-20 01:31:48 +0530

    TODO app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,80 @@
+import { useCallback, useEffect, useState } from "react";
 import AppStyle from "./App.module.css";
+import ToDo, { CardComponent } from "./components/todo/todo";
 
+interface TODODataProps {
+  id: string;
+  name: string;
+}
 function App() {
+  const [formData, setFormData] = useState<TODODataProps[]>([]);
+  const [editedValue, setEditedValue] = useState<string>("");
+  const [editId, setEditId] = useState<string | null>(null);
+
+  //TO catch todo data
+  const handleToDoData = useCallback(
+    (data: string) => {
+      if (editId) {
+        setFormData((prevData) =>
+          prevData.map((item) =>
+            item.id === editId ? { ...item, name: data } : item
+          )
+        );
+        setEditId(null);
+      } else {
+        setFormData((prevData) => [
+          ...prevData,
+          { id: new Date().toISOString(), name: data },
+        ]);
+      }
+      setEditedValue("");
+    },
+    [editId]
+  );
+  // To handle delete data
+  const handleDelete = useCallback((id: string) => {
+    setFormData((prevData) => prevData.filter((item) => item.id !== id));
+  }, []);
+
+  const editHandler = useCallback(
+    (id: string) => {
+      const item = formData.find((item) => item.id === id);
+      if (item) {
+        setEditedValue(item.name);
+        setEditId(id);
+      }
+    },
+    [formData]
+  );
   return (
     <main className={AppStyle["app-container"]}>
       <section className={AppStyle["app-container-item"]}>
+        {/* HEADING */}
         <h4>Interview question solution</h4>
+        <div className={AppStyle["app-container-section"]}>
+          {/* SECTION_1 */}
+          <section className={AppStyle["section-item"]}>
+            <ToDo
+              ToDoDatahandler={handleToDoData}
+              editValue={editedValue}
+              setEditedValue={setEditedValue}
+            />
+          </section>
+          {/* SECTION_2 */}
+          <div className={AppStyle["divider"]}></div>
+          {/* SECTION_3 */}
+          <section className={AppStyle["section-item"]}>
+            {formData.map((element) => (
+              <CardComponent
+                key={element.id}
+                name={element.name}
+                id={element.id}
+                onDelete={handleDelete}
+                onEdit={editHandler}
+              />
+            ))}
+          </section>
+        </div>
       </section>
     </main>
   );
---
+++ src/components/todo/todo.tsx
@@ -0,0 +1,79 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import ToDoStyle from "./todo.module.css";
+
+interface UserInputProps {
+  labelName: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+//Input component: We can also create in another file
+const UserInput = ({ labelName, value, onChange }: UserInputProps) => {
+  return (
+    <div className={ToDoStyle["custom-input"]}>
+      <label htmlFor="username">{labelName}</label>
+      <input name="user-name" value={value} onChange={onChange} />
+    </div>
+  );
+};
+//Main TODO Component
+interface ToDoProps {
+  ToDoDatahandler: (data: string) => void;
+  editValue: string;
+  setEditedValue: (value: string) => void;
+}
+const ToDo = (props: ToDoProps) => {
+  const [userInput, setUserInput] = useState<string>("");
+
+  const handleUserInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    props.setEditedValue(value);
+  };
+
+  const handSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    props.ToDoDatahandler(props.editValue);
+    props.setEditedValue("");
+  };
+  console.log("editevalue", props.editValue);
+  return (
+    <form className={ToDoStyle["todo-form"]} onSubmit={handSubmit}>
+      <UserInput
+        labelName="Enter Name"
+        value={props.editValue}
+        onChange={handleUserInput}
+      />
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+// Custom card component
+interface CardComponentProps {
+  id: string;
+  name: string;
+  onDelete: (id: string) => void;
+  onEdit: (id: string) => void;
+}
+export const CardComponent = ({
+  id,
+  name,
+  onDelete,
+  onEdit,
+}: CardComponentProps) => {
+  const handleDelete = (id: string) => {
+    onEdit(id);
+  };
+  const handleEdit = (id: string) => {
+    onDelete(id);
+  };
+  return (
+    <div className={ToDoStyle["custom-card"]}>
+      <h3>{name}</h3>
+      <button onClick={() => handleDelete(id)}>Delete</button>
+      <button onClick={() => handleEdit(id)}>Edit</button>
+    </div>
+  );
+};
+export default ToDo;
