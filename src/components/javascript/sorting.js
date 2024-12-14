ed497e7d6d41197512bbfd764bc41dc6b01e017f
Anurag k ray <anurag.k.ray@hotmail.com>
2024-12-14 22:28:44 +0530

    Update the selection sort

--- src/components/javascript/sorting.js
+++ src/components/javascript/sorting.js
@@ -13,12 +13,20 @@ console.log(numberArray);
 //Selection sort
 const numberArray_1 = [100,-1,99,-14,999,-5,500,34];
 for(let firstLoopIndex=0;firstLoopIndex<numberArray_1.length;firstLoopIndex++){
-    let getElement = numberArray_1[firstLoopIndex];
-    for(let secondLoopIndex=0;secondLoopIndex<firstLoopIndex+1;secondLoopIndex++){
-        if(getElement<numberArray_1[secondLoopIndex]){
-            [numberArray[firstLoopIndex], numberArray[secondLoopIndex]]=
-            [numberArray[secondLoopIndex],numberArray[firstLoopIndex]]
-        }
+    let getIndex = firstLoopIndex;
+    for (
+      let secondLoopIndex = firstLoopIndex + 1;
+      secondLoopIndex < numberArray_1.length;
+      secondLoopIndex++
+    ) {
+      if (numberArray_1[secondLoopIndex] < numberArray_1[getIndex]) {
+        getIndex = secondLoopIndex;
+      }
     }
+    //Swap inside the first loop
+    [numberArray_1[firstLoopIndex], numberArray_1[getIndex]] = [
+      numberArray_1[getIndex],
+      numberArray_1[firstLoopIndex],
+    ];
 }
 console.log(numberArray_1);
