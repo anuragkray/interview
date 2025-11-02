5f31c7528f4d674f1895d311a2e5a26ab8dd3267
Anurag K Ray <ianurag.k.ray@gmail.com>
2025-11-02 08:57:02 +0530

    Added the insertion sort

--- src/components/javascript/sorting.js
+++ src/components/javascript/sorting.js
@@ -24,13 +24,33 @@ console.log(ary_1);
 //OUTPUT : [ -14, -5, -1, 99, 100, 999 ]
 
 /* ⚙️ How Insertion Sort Works (Step-by-Step)
-  1.Start from the second element (index 1), assuming the first element is already sorted.
+  1.Start from the second element (index 1), 
+  assuming the first element is already sorted.
+  assume like first part is sorted and another is unsorted
   2.Compare the current element with the elements in the sorted portion (left side).
   3.Shift all larger elements one position to the right.
   4.Insert the current element into its correct position.
   5.Repeat until the entire array is sorted.
 */
 const ary_2 = [5, 3, 8, 4, 2];
+function insrtnSrt(param) {
+  for (let i = 1; i < param.length; i++) {
+    let current = param[i];
+    let prev = i - 1;
+
+    // Shift larger elements to the right
+    while (prev >= 0 && param[prev] > current) {
+      param[prev + 1] = param[prev];
+      prev--;
+    }
+
+    // Insert current value at the correct position
+    param[prev + 1] = current;
+  }
+
+  return param;
+}
+console.log(insrtnSrt(ary_2));
 
 
 //Selection sort
