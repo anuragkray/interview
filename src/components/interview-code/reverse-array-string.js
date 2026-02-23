1be99630f4940502e06e27afaa6c4d26f4b75c17
anuragkray <ianurag.k.ray@gmail.com>
2026-02-23 14:26:09 +0530

    Reverse array and string

--- src/components/interview-code/reverse-array-string.js
+++ src/components/interview-code/reverse-array-string.js
@@ -1,10 +1,48 @@
-//Reverse Array String
+//Reverse Array Nunber
+const numberArray = [1, 2, 3, 4, 5, 6];
 
-// Without any built-in method
-function reverse_1(params) {
-  for (let left = 0, right = params.length; left < right; left++, right--) {
+function reverseArray(params) {
+  for (let left = 0, right = params.length - 1; left < right; left++, right--) {
     [params[left], params[right]] = [params[right], params[left]];
   }
   return params;
 }
-console.log(reverse_1([1, 2, 3, 4, 5]));
+console.log(reverseArray(numberArray));
+
+//Reverse String using Inbuilt function
+function reverseUsingInBuilt(params) {
+  const reverse = [...params].reduceRight((acc, cur) => {
+    acc += cur;
+    return acc;
+  }, "");
+  return reverse;
+}
+const name_1 = "Mahakal Mahadev Bholenath";
+console.log(reverseUsingInBuilt(name_1));
+
+//Step-2
+function inBuilt2(params) {
+  return params.split("").reverse().join("");
+}
+console.log(inBuilt2(name_1));
+
+//Reverse String by word
+const name = "Mahakal Mahadev Bholenath";
+
+function reverseByWords(params) {
+  let words = "";
+  let reverseSentence = "";
+
+  for (let index = params.length - 1; index >= 0; index--) {
+    if (params[index] === " ") {
+      reverseSentence += " " + words;
+      words = "";
+    } else {
+      words = params[index] + words;
+    }
+  }
+  reverseSentence += " " + words;
+
+  return reverseSentence;
+}
+console.log(reverseByWords(name));
