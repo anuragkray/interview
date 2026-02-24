647be3892694051a7fd7d5048d11070b8af198fb
anuragkray <ianurag.k.ray@gmail.com>
2026-02-24 19:43:01 +0530

    RIA

---
+++ src/components/interview-code/reverse-by-words.js
@@ -0,0 +1,23 @@
+const data = "Har Har Mahadev Jay Bholenath";
+
+/**
+ * Input: Har Har Mahadev Jay Bholenath
+ * Output: Bholenath Jay Mahadev Har Har
+ */
+// Method 1: Reverse Iteration Algorithm
+function reverseByWords(sentence) {
+  let words = "";
+  let reverseResult = "";
+
+  for (let char = sentence.length - 1; char >= 0; char--) {
+    if (sentence[char] === " ") {
+      reverseResult += " " + words;
+      words = "";
+    } else {
+      words = sentence[char] + words;
+    }
+  }
+  reverseResult += " " + words;
+  return reverseResult;
+}
+console.log(reverseByWords(data));
