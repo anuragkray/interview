5377c65b25098e197bc13088074cb6ea8bfb1518
anuragkray <ianurag.k.ray@gmail.com>
2026-02-26 20:10:14 +0530

    Added the logic for the remove duplicate

--- src/components/interview-code/remove-duplicate.js
+++ src/components/interview-code/remove-duplicate.js
@@ -3,5 +3,17 @@
  * Output : "abcde"
  */
 // Without Inbuilt methods
+const data = "abcbcbdedbc";
+const withoutInbuiltMethod = (parameter) => {
+  const seen = {};
+  let result = "";
 
-const withoutInbuiltMethod = (parameter) => {};
+  for (let item of parameter) {
+    if (!seen[item]) {
+      seen[item] = true;
+      result += item;
+    }
+  }
+  console.log(result);
+};
+withoutInbuiltMethod(data);
